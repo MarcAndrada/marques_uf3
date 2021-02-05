842f5b37979b3aa7cfc273fe375a9a1e87735cd0
Marc <cramitus@gmail.com>
2021-02-05 14:35:34 +0100

    Creacion de Servidor web JS

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const http = require("http");
+const fs = require("fs")
+
+console.log("Inicializando el servidor web");
+
+http.createServer((request, response) => {
+console.log(request.url);
+if (request.url == "/stars.jpg"){
+	fs.readFile("stars.jpg", (error, content) => {
+
+	});
+}else{
+	fs.readFile("index.html", (error, content) => {
+	response.writeHead(200,{'Content-Type': 'text/html'});
+	response.write(content);
+	response.end();
+}); 
+}
+}).listen(8080);
+
